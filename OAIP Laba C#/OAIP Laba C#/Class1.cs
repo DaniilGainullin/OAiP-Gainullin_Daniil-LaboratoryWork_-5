021ac8d9ca008e95039719b773d5e41352543781
Daniil <danya.gaynullin@bk.ru>
2026-02-16 15:24:44 +0300

    first commit

---
+++ OAIP Laba C#/OAIP Laba C#/Class1.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OAIP_Laba_C_
+{
+    public class Linear
+    {
+            public double first;
+            public double second;
+            public double Function()
+            {
+                return -second / first;
+            }
+        }
+    }
+
---
+++ OAIP Laba C#/OAIP Laba C#/Program.cs
@@ -0,0 +1,18 @@
+﻿
+using OAIP_Laba_C_;
+
+class Uravnenie
+{
+    static void Main()
+    {
+        Linear linear = new Linear();
+        Console.WriteLine("Введите число А");
+        linear.first = double.Parse(Console.ReadLine());
+        Console.WriteLine("Введите число В");
+        linear.second = double.Parse(Console.ReadLine());
+        double x = linear.Function();
+        Console.WriteLine("Корень:" + x);
+    }
+
+
+}
